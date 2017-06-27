207057e5d28d34feab9f133ed0611de34b529a61
Tyler Murray <TWMurray@fullsail.edu>
2017-06-27 18:46:10 -0400

    Adding Base project to git

---
+++ App7/App7/App7Main.h
@@ -0,0 +1,41 @@
+﻿#pragma once
+
+#include "Common\StepTimer.h"
+#include "Common\DeviceResources.h"
+#include "Content\Sample3DSceneRenderer.h"
+#include "Content\SampleFpsTextRenderer.h"
+
+// Renders Direct2D and 3D content on the screen.
+namespace App7
+{
+	class App7Main : public DX::IDeviceNotify
+	{
+	public:
+		App7Main(const std::shared_ptr<DX::DeviceResources>& deviceResources);
+		~App7Main();
+		void CreateWindowSizeDependentResources();
+		void Update();
+		bool Render();
+
+		///
+		void OnPointerPressed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
+		void OnPointerReleased(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
+		void OnPointerMoved(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args);
+		///
+
+		// IDeviceNotify
+		virtual void OnDeviceLost();
+		virtual void OnDeviceRestored();
+
+	private:
+		// Cached pointer to device resources.
+		std::shared_ptr<DX::DeviceResources> m_deviceResources;
+
+		// TODO: Replace with your own content renderers.
+		std::unique_ptr<Sample3DSceneRenderer> m_sceneRenderer;
+		std::unique_ptr<SampleFpsTextRenderer> m_fpsTextRenderer;
+
+		// Rendering loop timer.
+		DX::StepTimer m_timer;
+	};
+}
